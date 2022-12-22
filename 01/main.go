93df176c3c1dbe04c88f8c7ecee15aee49afd26c
jobcher <13028911306@163.com>
2022-12-22 15:36:10 +0800

    update

---
+++ 01/main.go
@@ -0,0 +1,11 @@
+// main包
+package main
+
+import "fmt"
+
+// 这是main函数
+// 是程序启动的入口
+
+func main() {
+	fmt.Println("hello jobcher!!")
+}
---
+++ 01/var.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var version string = "1.0"
+
+func main() {
+	var me string
+	me = "jobcher"
+
+	fmt.Println(me)
+	fmt.Println(version)
+}
