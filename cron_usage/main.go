ddafebc7d2d1b3f101f4fd1ed2e6a550946f8053
jobcher <jobcher@github.com>
2023-01-05 10:29:12 +0800

    更新

--- cron_usage/main.go
+++ cron_usage/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gorhill/cronexpr"
 )
 
 func main() {
 	var (
-		expr *cronexpr.Expression
-		err  error
+		expr     *cronexpr.Expression
+		err      error
+		now      time.Time
+		nextTime time.Time
 	)
 
-	if expr, err = cronexpr.Parse("* * * * *123"); err != nil {
+	if expr, err = cronexpr.Parse("*/5 * * * * * *"); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	expr = expr
+	now = time.Now()
+	nextTime = expr.Next(now)
+	time.AfterFunc(nextTime.Sub(now), func() {
+		fmt.Println("被调度了：", nextTime)
+	})
+	time.Sleep(5 * time.Second)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-test
 
 go 1.19
 
-require github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75 // indirect
+require github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75
