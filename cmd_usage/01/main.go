a64d2a899d10deaeabc442cbbc4c689237802340
jobcher <jobcher@github.com>
2023-01-05 10:01:08 +0800

    更新

--- cmd_usage/01/main.go
+++ cmd_usage/01/main.go
@@ -6,5 +6,11 @@ import "fmt"
 // 是程序启动的入口
 
 func main() {
+	var version string = "1.0"
 	fmt.Println("hello jobcher!!")
+	var me string
+	me = "jobcher"
+
+	fmt.Println(me)
+	fmt.Println(version)
 }
