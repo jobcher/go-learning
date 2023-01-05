082a3bb2ace91c289a932e8d0c5ac0ceb3df0ab6
jobcher <jobcher@github.com>
2023-01-05 10:09:51 +0800

    新增cron

--- cron_usage/main.go
+++ cron_usage/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/gorhill/cronexpr"
+)
+
+func main() {
+	var (
+		expr *cronexpr.Expression
+		err  error
+	)
+
+	if expr, err = cronexpr.Parse("* * * * *123"); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expr = expr
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-test
 
 go 1.19
+
+require github.com/gorhill/cronexpr v0.0.0-20180427100037-88b0669f7d75 // indirect
