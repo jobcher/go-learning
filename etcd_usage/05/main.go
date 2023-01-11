35812c86dabf8a4489922a04e66d0ef93154219d
jobcher <13028911306@163.com>
2023-01-11 14:44:37 +0800

    更新配置

--- etcd_usage/05/main.go
+++ etcd_usage/05/main.go
@@ -1,5 +1,93 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
 
+	"github.com/coreos/etcd/clientv3"
+)
+
+// 集群列表
+const ETCD_URL = "127.0.0.1:2379"
+
+func main() {
+	var (
+		config         clientv3.Config
+		client         *clientv3.Client
+		err            error
+		lease          clientv3.Lease
+		leaseGrantResp *clientv3.LeaseGrantResponse
+		leaseId        clientv3.LeaseID
+		kv             clientv3.KV
+		putResp        *clientv3.PutResponse
+		getResp        *clientv3.GetResponse
+		keepResp       *clientv3.LeaseKeepAliveResponse
+		keepRespChan   <-chan *clientv3.LeaseKeepAliveResponse
+	)
+
+	config = clientv3.Config{
+		Endpoints:   []string{ETCD_URL}, // 集群列表
+		DialTimeout: 5 * time.Second,
+	}
+
+	// 建立一个客户端
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//申请lease
+	lease = clientv3.NewLease(client)
+	//10s
+	if leaseGrantResp, err = lease.Grant(context.TODO(), 10); err != nil {
+		fmt.Println(err)
+		return
+	}
+	//获取租约id
+	leaseId = leaseGrantResp.ID
+
+	//自动续租
+	if keepRespChan, err = lease.KeepAlive(context.TODO(), leaseId); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go func() {
+		for {
+			select {
+			case keepResp = <-keepRespChan:
+				if keepRespChan == nil {
+					fmt.Println("租约已经失效")
+					goto END
+				} else {
+					fmt.Println("收到自动续租应答", keepResp.ID)
+				}
+			}
+		}
+	END:
+	}()
+
+	//获取kv
+	kv = clientv3.NewKV(client)
+	//put kv,关联租约
+	if putResp, err = kv.Put(context.TODO(), "/cron/lock/job1", "", clientv3.WithLease(leaseId)); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("写入成功", putResp.Header.Revision)
+
+	//定时查看key是否过期
+	for {
+		if getResp, err = kv.Get(context.TODO(), "/cron/lock/job1"); err != nil {
+			fmt.Println(err)
+			return
+		}
+		if getResp.Count == 0 {
+			fmt.Println("kv过期")
+			break
+		}
+		fmt.Println("还没过期", getResp.Kvs)
+		time.Sleep(2 * time.Second)
+	}
 }
